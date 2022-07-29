016b64f111cb015e2cb2f68b48f59ab1725bcf57
Paferreira982 <paferreira982@hotmail.com>
2022-07-29 14:01:14 -0300

    [FIX] Removendo sync do sequelize

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,11 @@
 import express from 'express'
 
-import Database from './app/database'
-import sequelize from './app/database/SequelizeConfig'
-
 class App {
   public express: express.Application
 
   public constructor () {
     this.express = express()
-    this.database()
     this.middlewares()
-  }
-
-  private database (): void {
-    Database.createDatabase()
-      .then(async () => {
-        await sequelize.sync()
-      })
   }
 
   private middlewares (): void {
