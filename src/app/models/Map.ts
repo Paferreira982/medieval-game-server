1225a198d5c531aa819a86a4426074e6fdf15e03
Paferreira982 <paferreira982@hotmail.com>
2022-08-01 17:16:48 -0300

    [MINOR] Mapeamento da entidade: Map

---
+++ src/app/models/Map.ts
@@ -0,0 +1,32 @@
+import { Model, DataTypes } from 'sequelize'
+import sequelize from '../database/SequelizeConfig'
+
+class Map extends Model {
+  declare uuid: string
+}
+
+Map.init(
+  {
+    uuid: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true
+    },
+
+    name: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false
+    },
+
+    image: {
+      type: DataTypes.BLOB
+    }
+  },
+  {
+    tableName: 'maps',
+    sequelize
+  }
+)
+
+export default Map
